a736e9ee7b555780fcf209e51e2becd04565f24f
AslamHamdi <wokle08@gmail.com>
2023-02-15 00:22:49 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+// import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   test: {
     environment: 'happy-dom', 
   },
-  plugins: [vue()],
+  plugins: [vue()], //, dts()
   build: {
     lib: {
       entry: './src/plugin.ts', //path.resolve(__dirname, '/src/plugin.ts'),
